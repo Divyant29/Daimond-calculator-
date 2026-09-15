680f7ceffcc941a4592d7420d40f9bba242518da
Divyant Parmar <140545694+Divyant29@users.noreply.github.com>
2026-09-15 23:01:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CHANGE THIS VERSION NUMBER EVERY TIME YOU PUSH TO GITHUB
-const CACHE_NAME = 'diamond-calc-v1.0.3'; 
+const CACHE_NAME = 'diamond-calc-v1.0.4'; 
 
 const ASSETS_TO_CACHE = [
   './',
