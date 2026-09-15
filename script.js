eee4243c2f2287d8540617b4b9911d4f7fcb4b72
Divyant Parmar <140545694+Divyant29@users.noreply.github.com>
2026-09-15 13:15:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Holds the currently displayed/filtered report data, used by generatePDFReport()
- let currentReportStones = [];
+
 
 // --- FULL APP I18N DICTIONARY --
 const i18n = {
@@ -656,7 +655,7 @@ async function saveStone() {
    if (sortVal === 'val') return b.totalVal - a.totalVal;
    return 0;
   });
-  currentReportStones = stones;
+  
   let h = '', tC = 0, tW = 0, tV = 0;
   stones.forEach(s => { 
    tC++; tW += s.weight; tV += s.totalVal; 
