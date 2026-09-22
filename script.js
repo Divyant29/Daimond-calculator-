81d6aafc7387bfb36eec886ba3e35774d474fbdb
Divyant Parmar <140545694+Divyant29@users.noreply.github.com>
2026-09-22 14:10:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -152,6 +152,7 @@ const i18n = {
  const isIOS = /iphone|ipad|ipod/.test(navigator.userAgent.toLowerCase());
 const isStandalone = window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone;
 const alreadyInstalled = localStorage.getItem('appInstalled') === 'true';
+showToast('standalone:' + isStandalone + ' | installed:' + alreadyInstalled, 'info', 10000);
 
 window.addEventListener('beforeinstallprompt', (e) => {
  e.preventDefault();
